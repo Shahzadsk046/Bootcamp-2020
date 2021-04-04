43cd84b225f36abe2f4bb41de9ef74a3789b0166
Shehzad Khan <shahzad.sk046@gmail.com>
2021-04-05 01:45:07 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className = 'App-header'>
+        <h1>Assignment # 3 by Shehzad Khan</h1>
       <Book name = "C++" author = "Thomas" />
       <Book name = "Python" author = "David Ascher" />
       <Book name = "Javascript" author = "Marjin Haverbeke" />
