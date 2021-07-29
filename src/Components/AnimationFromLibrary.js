1e384bc10a2fe7be485c0593716a883ecd01d702
Shehzad Khan <55661874+Shahzadsk046@users.noreply.github.com>
2021-07-29 21:24:38 +0500

    AnimationFromLibrary.js File Created

---
+++ src/Components/AnimationFromLibrary.js
@@ -0,0 +1,49 @@
+import useWebAnimations, {
+  bounce,
+  rubberBand,
+} from "@wellyshen/use-web-animations";
+
+function Animations() {
+
+  const { ref, playState, getAnimation } = useWebAnimations(
+    { ...bounce }
+    // {...rubberBand},
+
+    //   {
+    //   keyframes: [
+    //     { transform: "translate(0,0)" },
+    //     { transform: "translate(600px,0)" },
+    //   ],
+    // animationOptions: {
+    //   delay: 4000, // Start with a 500ms delay
+    //       duration: 1000, // Run for 1000ms
+    // iterations: Infinity, // Repeat once
+    // direction: "alternate", // Run the animation forwards and then backwards
+    // easing: "ease-in-out", // Use a fancy timing function
+    // },
+    //   // onReady: ({ playState, animate, animation }) => {
+    //   //   // Triggered when the animation is ready to play
+    //   // },
+    //   // onUpdate: ({ playState, animate, animation }) => {
+    //   //   // Triggered when the animation enters the running state or changes state
+    //   // },
+    //   // onFinish: ({ playState, animate, animation }) => {
+    //   //   // Triggered when the animation enters the finished state
+    //   // },
+    //   // // More useful options...
+    // }
+  );
+
+  return (
+    <div>
+      <h1>Web Animation App Using React</h1>
+      {/* <input ref={element} type="text" /> */}
+      <div className="target" ref={ref}></div>
+      Current Animation State: {playState}
+      <button onClick={() => getAnimation().pause()}>Pause</button>
+      <button onClick={() => getAnimation().play()}>Play</button>
+    </div>
+  );
+}
+
+export default Animations;
